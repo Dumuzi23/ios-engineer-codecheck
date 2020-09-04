1aedfa9cf14bc30958609900ccb12791aa6e8003
ta <amida.tatsuya@gmail.com>
2020-09-04 23:24:41 +0900

    add-RepositoryData.swiftを追加

---
+++ iOSEngineerCodeCheck/Model/RepositoryData.swift
@@ -0,0 +1,27 @@
+//
+//  RepositoryData.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by Tatsuya Amida on 2020/09/04.
+//  Copyright © 2020 YUMEMI Inc. All rights reserved.
+//
+
+import Foundation
+
+struct RepositoryData: Codable {
+    let items: Items
+}
+
+struct Items: Codable {
+    let full_name: String
+    let owner: Owner
+    let stargazers_count: Int
+    let watchers_count: Int
+    let language: String
+    let forks_count: Int
+    let open_issues: Int
+}
+
+struct Owner: Codable {
+    let avatar_url: URL
+}
